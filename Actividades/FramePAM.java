0333e069caf7acb0aa29f0b17a37c92dabebc3bd
124051297 <124051297@upq.edu.mx>
2025-09-04 18:18:32 -0600

    Creación de una clase con sus 4 metodos corespondientes

---
+++ Actividades/FramePAM.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+public class FramePAM  extends JFrame
+{
+ 
+
+     
+ public static void main (String[] args)
+ {
+       
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300,300);
+        frame.setVisible(true);
+
+        
+
+ }
+  
+
+}
