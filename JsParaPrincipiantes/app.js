1b662083dcafb84092ac30adca50109226e759e5
124051297 <124051297@upq.edu.mx>
2025-09-09 15:34:00 -0600

    Tipos de datos y uso de String tanto en HTML como en JS

---
+++ JsParaPrincipiantes/app.js
@@ -0,0 +1,33 @@
+document.write("<h1>Hola mundo</h1>")
+
+// Tipos de datos
+ "Hellow world" //String con comillas doble
+ 'Hellow world' //String con comillas simples
+
+ console.log('Hellow world');
+
+ // Numericos
+ 1000 // Int
+ -2.3
+
+ // Boleano
+ tru 
+ false
+
+ // Arrays
+
+ ['jose','ryan','martha']
+ [1,2,3,4]
+ [true,false,true,false]
+
+ // Objeto
+
+ 'ryan'
+ 70.4
+ true
+ {
+     "username": 'ryan',
+     "score": 70.4,
+     "horas": 14,
+     "profecional": true
+ }
