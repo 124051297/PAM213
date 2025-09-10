e34d1ce8e21db54e9cdacab8beb3fae004508047
124051297 <124051297@upq.edu.mx>
2025-09-10 14:19:32 -0600

    Culminacion del video Js para principiantes, uso de ciclos, declaracion de variables y declaracione if/else

--- JsParaPrincipiantes/app.js
+++ JsParaPrincipiantes/app.js
@@ -31,3 +31,119 @@ document.write("<h1>Hola mundo</h1>")
      "horas": 14,
      "profecional": true
  }
+
+ {
+  "username": 'joe',
+  "score": 0.4,
+  "hours": 1,
+  "proffesional": false
+}
+
+
+console.log("esto es una cadena")
+console.log("14.3")
+console.log("true")
+console.log(["1,2,3"])
+ 
+/un objeto empieza con llaves
+console.log({"username": "ryan", "score": 70.4});
+
+//console.log(completeName);
+//concatenar 
+
+let numberOne = 100;
+let numberTwo = 500;
+
+let result = numberOne > numberTwo;
+
+//console.log(result);
+
+let passwordDB = 'pepe123';
+let input = 'pepe123';
+
+let result = input == passwordDB;
+
+console.log(result);
+
+var nombre = "john";
+length lastname = "john";
+
+nombre = "yoyo";
+4  nombre e';
+6  const PI = 3.1415;
+7  PI = 100;
+9  console.log(PI)
+
+console.log(nombre);
+4  nameuser = 'pepe';
+6  const PI = 3.1415;
+7  PI = 100;
+9  console.log(PI)
+ 
+// camelcase
+let nombre = "gordon"; 
+
+let numberOne = 60;
+let numberTwo = 100;
+let result = numberOne | numberTwo;
+// ++++++++
+console.log(result);
+let numberOne = 100;
+
+let name = 'trala';
+let lastname = 'Carter';
+
+let completeName = name + ' ' + lastname;
+//console.log(completeName);
+//concatenar 
+
+let numberOne = 100;
+let numberTwo = 500;
+
+let result = numberOne > numberTwo;
+
+//console.log(result);
+
+let passwordDB = 'pepe123';
+let input = 'pepe123';
+
+let result = input == passwordDB;
+
+console.log(result);
+
+
+//condicional
+let score = 70;
+if (score > 30) {
+    console.log('You need to practice more');
+} else if (score > 15) {
+    console.log('Estas mejorando');
+}
+else {
+    console.log('You need to follow this tutorial');
+}
+
+
+
+// bucles 
+//let count = 0;
+//while (count < 50) {
+    //console.log(count);
+  //  count++;
+//}
+//let names = ['ryan', 'joe', 'john'];
+
+//for(let i = names.length - 1; i >= 0; i--) {
+   // console.log(names[i]);
+//}
+
+
+// funcionesfunction add(n1, n2) {
+ //   console.log(n1 + n2);
+//}
+add(3, 2);
+add(100, 300);
+
+add(3, 2);
+add(100, 300);
+add(3123123123, 132123123);
