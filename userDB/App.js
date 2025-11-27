9a8be792e024fa28f9a782b3d45e81ad39025259
sebastian <124051297@upq.edu.mx>
2025-11-26 23:12:08 -0600

    Practica No.19: Insert y Select con SQLite Local

---
+++ userDB/App.js
@@ -0,0 +1,12 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import InsertUsuarioScreen from './screen/InsertUsuarioScreen';
+export default function App() {
+  return (
+
+      <InsertUsuarioScreen/>
+   
+  );
+}
+
+
