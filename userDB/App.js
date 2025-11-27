3b20e06421a3e21dda016683854802becf02ae4a
sebastian <124051297@upq.edu.mx>
2025-11-26 23:23:48 -0600

    Correccion del la practica 19

--- userDB/App.js
+++ userDB/App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import InsertUsuarioScreen from './screen/InsertUsuarioScreen';
+
+
 export default function App() {
   return (
-
+      
       <InsertUsuarioScreen/>
+      
    
   );
 }
